f2c30c88c7615f1cead9c3ad25d4c8cfb939c85c
Klepnev <klepnev-ea@yandex.ru>
2016-04-22 16:07:02 +0300

    new

--- js/js.js
+++ js/js.js
@@ -1,3 +1,4 @@
+(function() {
  new Promise(function(resolve) {
     if(document.readyState === 'complete') {
         resolve();
@@ -8,51 +9,13 @@
  }).then(function() {
        
     ymaps.ready(function () {
-            var myPlacemark,
-                myMap = new ymaps.Map('map', {
-                center: [-84.50676726894993, 32.09877903696875],
-                zoom: 9,
-                behaviors: ['default', 'scrollZoom']
-            }, {
-                searchControlProvider: 'yandex#search'
-            }),
- 
-            // Создаем собственный макет с информацией о выбранном геообъекте.
-                customItemContentLayout = ymaps.templateLayoutFactory.createClass(
-                // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
-                '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
-                '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
-                '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
-            ),
-                clusterer = new ymaps.Clusterer({
-                clusterDisableClickZoom: true,
-                clusterOpenBalloonOnClick: true,
-                // Устанавливаем стандартный макет балуна кластера "Карусель".
-                clusterBalloonContentLayout: 'cluster#balloonCarousel',
-                // Устанавливаем собственный макет.
-                clusterBalloonItemContentLayout: customItemContentLayout,
-                // Устанавливаем режим открытия балуна. 
-                // В данном примере балун никогда не будет открываться в режиме панели.
-                clusterBalloonPanelMaxMapArea: 0,
-                // Устанавливаем размеры макета контента балуна (в пикселях).
-                clusterBalloonContentLayoutWidth: 200,
-                clusterBalloonContentLayoutHeight: 130,
-                // Устанавливаем максимальное количество элементов в нижней панели на одной странице
-                clusterBalloonPagerSize: 5
-                // Настройка внешего вида нижней панели.
-                // Режим marker рекомендуется использовать с небольшим количеством элементов.
-                // clusterBalloonPagerType: 'marker',
-                // Можно отключить зацикливание списка при навигации при помощи боковых стрелок.
-                // clusterBalloonCycling: false,
-                // Можно отключить отображение меню навигации.
-                // clusterBalloonPagerVisible: false
-            });
 
+            var coords;
             var xhr = new XMLHttpRequest();
             xhr.responseType = 'json';
             xhr.open('post', 'http://localhost:3000/', true);
             xhr.onload = function() {
-
+                        console.log(xhr.response);
                 for(var k in xhr.response) {
                     var reviews = xhr.response[k];
                     reviews.forEach(function(review) {
@@ -61,6 +24,40 @@
                 }
             };
             xhr.send(JSON.stringify({op: 'all'}));
+
+            var dataMap = {
+                coords: [],
+                name: '',
+                place: '',
+                text: '',
+                address: ''
+            }
+
+            var myPlacemark,
+                myMap = new ymaps.Map('map', {
+                center: [51.52420326872876, 46.04014127613009],
+                zoom: 9,
+                behaviors: ['default', 'scrollZoom']
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+ 
+                customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+                '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
+                '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+            ),
+                clusterer = new ymaps.Clusterer({
+                clusterDisableClickZoom: true,
+                clusterOpenBalloonOnClick: true,
+                clusterBalloonContentLayout: 'cluster#balloonCarousel',
+                clusterBalloonItemContentLayout: customItemContentLayout,
+                clusterBalloonPanelMaxMapArea: 0,
+                clusterBalloonContentLayoutWidth: 200,
+                clusterBalloonContentLayoutHeight: 130,
+                clusterBalloonPagerSize: 5
+            });
+
 
 
             getPointData = function (index) {
@@ -76,14 +73,14 @@
                 };
             },
             points = [
-                [55.831903,37.411961]
+                
             ],
             geoObjects = [];
 
 
             for(var i = 0, len = points.length; i < len; i++) {
                 geoObjects[i] = new ymaps.Placemark(points[i], getPointData(i), getPointOptions());
-            }
+            };
 
 
             clusterer.options.set({
@@ -96,78 +93,72 @@
             myMap.geoObjects.add(clusterer);
 
 
-            myMap.setBounds(clusterer.getBounds(), {
-                checkZoomRange: true
-            });
-
-
             // Слушаем клик на карте
             myMap.events.add('click', function (e) {
-                var coords = e.get('coords');
+                dataMap.coords = e.get('coords');
+                popup.style.display = 'block';
 
-
-
-                getAddress(coords).then(function(gotAddress) {
- 
-                    var address =  gotAddress.properties.get('text');   
-
-
-                    popup.style.display = 'block';
-                    popup.style.top = e.clientY + 'px';
-                    popup.style.left = e.clientX + 'px';
-                    document.getElementById('address').innerText = address;                    
-
-                    if(popup.offsetTop + popup.offsetHeight > map.offsetHeight) {
-                        popup.style.top = '10px';
-                    }
-
-                    if(popup.offsetLeft + popup.offsetWidth > map.offsetWidth) {
-                        popup.style.left = popup.offsetLeft - popup.offsetWidth + 'px';
-                    } 
-
-                                   
-
-                    popup.addEventListener('click', function(e) {
-                        if(e.target.value == 'Ваше имя' || e.target.value == 'Укажите место' || e.target.value == 'Поделитесь впечатлениями') {
-                            e.target.value = '';    
-                        };
-
-                        if(e.target.id == 'closePopup') {
-                           closePopup();  
-                        } 
-
-                        var name = document.getElementById('name').value;
-                        var place = document.getElementById('place').value;
-                        var text = document.getElementById('text').value;
-
-
-                        if(e.target.id == 'submit' && name != 'Ваше имя' && place != 'Укажите место' && text != 'Поделитесь впечатлениями') {
-                           mark(coords, gotAddress.properties.get('text'), name, place, text);
-                           closePopup();
-
-                            var xhr = new XMLHttpRequest();
-                            xhr.open('post', 'http://localhost:3000/', true);
-                            xhr.onloadend = function() {
-                                console.log(xhr.response);
-                            };
-                            xhr.send(JSON.stringify({
-                                op: "add",
-                                review: {
-                                    coords: {x: coords[0], y: coords[1]},
-                                    address: gotAddress.properties.get('text'),
-                                    name: name,
-                                    place: place,
-                                    text: text 
-                                }
-                            }))   
-                        }                
-                    });
-                    myPlacemark = createPlacemark(coords);
-
+                getAddress(dataMap.coords).then(function(gotAddress) {
+                    dataMap.address = gotAddress.properties.get('text');
+                    document.getElementById('address').innerText = dataMap.address;
                 });
- 
+
                 
             });
+
+            popup.addEventListener('click', function(e) {
+               console.log(dataMap.coords);
+
+                if(e.target.id === 'submit' && name !== 'Ваше имя' && place !== 'Укажите место' && text !== 'Поделитесь впечатлениями') {
+
+                    dataMap.name = document.getElementById('name').value;
+                    dataMap.place = document.getElementById('place').value;
+                    dataMap.text = document.getElementById('text').value;
+
+                    myPlacemark = createPlacemark(dataMap.coords);   
+                    myPlacemark.properties
+                        .set({
+                            balloonContentHeader: dataMap.place,
+                            balloonContentBody: dataMap.text,
+                            balloonContentFooter: dataMap.name
+                    });
+                    clusterer.add(myPlacemark);                    
+
+
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('post', 'http://localhost:3000/', true);
+                    xhr.onloadend = function() {
+                        console.log(xhr.response);
+                    };
+                    xhr.send(JSON.stringify({
+                        op: "add",
+                        review: {
+                            coords: {x: dataMap.coords[0], y: dataMap.coords[1]},
+                            address: dataMap.address,
+                            name: dataMap.name,
+                            place: dataMap.place,
+                            text: dataMap.text 
+                        }
+                    }));  
+
+
+
+                    closePopup();                                     
+                };
+
+                if(e.target.value == 'Ваше имя' || e.target.value == 'Укажите место' || e.target.value == 'Поделитесь впечатлениями') {
+                    e.target.value = '';    
+                };
+
+                if(e.target.id == 'closePopup') {
+                   closePopup();  
+                } 
+
+
+            });
+
+
+
 
             // Создание метки
             function createPlacemark(coords) {
@@ -181,17 +172,14 @@
 
             // Определяем адрес по координатам (обратное геокодирование)
             function getAddress(coords) {
-                //myPlacemark.properties.set('iconContent', 'поиск...');
                 return ymaps.geocode(coords).then(function (res) {
                     return res.geoObjects.get(0);
-
- 
                 });
             }
 
-            function mark(coords, address, name, place, text) {
-                console.log(myPlacemark.properties); 
-                //myPlacemark = createPlacemark(coords);   
+           function mark(coords, address, name, place, text) {
+
+                myPlacemark = createPlacemark(coords);   
                 myPlacemark.properties
                     .set({
                         balloonContentHeader: place,
@@ -199,7 +187,7 @@
                         balloonContentFooter: name
                 });
                 clusterer.add(myPlacemark);
-                myPlacemark = createPlacemark(coords);
+                //myPlacemark = createPlacemark(coords);
             };
 
 
@@ -217,25 +205,6 @@
 
         });
  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}());
 
 
